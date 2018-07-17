3b99c23f2fec80a34937796723fce306eece0d1c
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-17 17:21:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,3 +112,46 @@ client.on('message', message => {
     })
     }
     })
+
+
+
+
+
+const adminprefix = "$vip";
+const devs = ['443852053882601504','452785037511753728' ];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`);
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then;
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`);
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`);
+}
+
+});
+
+
+
+client.on("message", message => {
+ if (message.content === `${prefix}vhelp`) {
+ 	
+  const embed = new Discord.RichEmbed() 
+      .setColor("#000000")
+      .setDescription(`
+${prefix}setgame ⇏ لي تغير حالة بوت 
+${prefix}setnme ⇏ تغير اسم بوت
+${prefix}setT ⇏ لي تغير حالة بوت لي تشويتش او حالة بنفسجية
+${prefix}setavatar ⇏ تغير صورة بوت
